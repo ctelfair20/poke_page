9f0f79d41ea0b5eb7d5607f6ad2e0420c754cf15
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-08-23 15:16:59 -0500

    adds stats component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
 import Card from './Card';
-
+import Stats from './Stats';
 function App() {
   return (
     <div className="App">
       <h1>Pokemon Page</h1>
       <main>
-        <Card />
+        <div className="left-col">
+          <Card />
+        </div>
+        <div className="rigth-col">
+          <Stats />
+        </div>
       </main>
     </div>
   );
---
+++ src/Stats.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+interface Props {
+}
+
+function Stats(props: Props) {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default Stats;
