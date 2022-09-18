e7a18c75829a792d50abe617ed38e01e2901fee5
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-09-18 13:14:59 -0500

    fresh eyes

--- src/Pokeball.tsx
+++ src/Pokeball.tsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 
 interface Props {
 
 }
 
+interface Pokemon {
+  species: any,
+}
+
 const Pokeball = (props: Props) => {
+  const randomNumber = (): number => {
+    return Math.floor(Math.random() * 905) + 1;
+  }
+
+  const handleOnClick = async (): Promise<Array<Pokemon>> => {
+    const { data } = await axios.get<Pokemon>(`https://pokeapi.co/api/v2/pokemon/${randomNumber()}`);
+    console.log(data);
+    return data;
+  }
+
   return (
-    <div className="pokeball-button">choose your pokemon!</div>
+    <button className="pokeball-button" onClick={handleOnClick}>choose your pokemon!</button>
   )
 }
 
