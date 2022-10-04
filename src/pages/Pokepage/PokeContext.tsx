577b06e45986801ef51723b6286087e959483c32
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-10-04 16:46:58 -0500

    add custom hooks to context

--- src/pages/Pokepage/PokeContext.tsx
+++ src/pages/Pokepage/PokeContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useContext, useState } from 'react';
 import { pokeInterface } from './interface';
 
 // tells the component what its props should look like
@@ -11,9 +11,14 @@ interface Props {
 
 // This context holds the data retrieved from the api -- not sure why i don't have to tell the code that is empty obj is of type pokeInterface
 export const PokeContext = createContext({});
+
 // This context holds the function needed to update the data from the api
 // "pokemon" is the input variable for the setter. Remember to set the variable's type based on the state it is trying to replace
 export const UpdatePokeContext = createContext((pokemon: pokeInterface) => { });
+
+// custom hooks
+export const usePokeContext = () => useContext(PokeContext);
+export const useUpdatePokeContext = () => useContext(UpdatePokeContext);
 
 const PokeProvider = ({ children }: Props) => {
 
