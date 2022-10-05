fd22e7ae0e1d3bcb8cc83bc9a2e9219c51397abc
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-10-04 22:50:49 -0500

    increase readability

--- src/components/PokeNameAndDescription.tsx
+++ src/components/PokeNameAndDescription.tsx
@@ -4,12 +4,13 @@ import { usePokeContext } from '../pages/Pokepage/PokeContext';
 const PokeNameAndDescription = () => {
 
   const { id, name, types, height, weight } = usePokeContext();
-
+  const typeName = types?.[0].type.name;
+  // types?.[0].type.name[0] + types?.[0].type.name.split('')slice(1)
   return (
     <Container>
       <Box className="pokemon-name">Name: {name} </Box>
       <Box className="pokemon-description">
-        Description: Number: {id} Types:{types?.[0].type.name} Height: {height} Weight:{weight}
+        Description: Number: {id} Types:{typeName} Height: {height} Weight:{weight}
       </Box>
     </Container>
   );
