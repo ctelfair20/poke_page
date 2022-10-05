ad0654863e96e814f220b1aed6d42da014b3ecdb
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-10-04 22:40:08 -0500

    increased readability

--- src/components/PokeImage.tsx
+++ src/components/PokeImage.tsx
@@ -4,10 +4,11 @@ import { Container } from '@mui/material';
 const PokeImage = () => {
 
   const { sprites, name } = usePokeContext();
+  const frontArtwork = sprites?.other['official-artwork']['front_default'];
 
   return (
     <Container className="pokemon-image-box">
-      <img src={sprites?.other['official-artwork']['front_default']} alt={name} className="pokemon-image" />
+      <img src={frontArtwork} alt={name} className="pokemon-image" />
     </Container>
   );
 }
