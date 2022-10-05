57aff9dae24f46243bd141b1637162f3a6b4a1f5
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-10-04 22:08:10 -0500

    display abilities clean up

--- src/components/Abilities.tsx
+++ src/components/Abilities.tsx
@@ -1,14 +1,9 @@
 import { Box } from '@mui/material';
 import { usePokeContext } from '../pages/Pokepage/PokeContext';
 
-interface Props {
-}
-
-const Abilities = (props: Props) => {
+const Abilities = () => {
 
   const { abilities } = usePokeContext();
-  // console.log('from abilities', pokemon)
-  // const { abilities } = usePokeContext();
 
   const abilitesMap = abilities?.map((ability, i) => {
     return <Box key={'ab' + i}>{ability.ability.name[0].toUpperCase() + ability.ability.name.slice(1)}</Box>
