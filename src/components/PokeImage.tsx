e519cf7157e73ef46dcc55995ec50b928a120e1d
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-10-04 22:23:46 -0500

    display pokeImage

--- src/components/PokeImage.tsx
+++ src/components/PokeImage.tsx
@@ -1,10 +1,9 @@
-import { useContext } from 'react'
-import { PokeContext } from '../pages/Pokepage';
+import { usePokeContext } from '../pages/Pokepage/PokeContext';
 import { Container } from '@mui/material';
 
 const PokeImage = () => {
 
-  const { sprites, name } = useContext(PokeContext)
+  const { sprites, name } = usePokeContext();
   return (
     <Container className="pokemon-image-box">
       <img src={sprites?.other['official-artwork']['front-default']} alt={name} className="pokemon-image" />
