5807c448599e55538813b311216d6b532e6dbbc9
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-10-04 21:49:18 -0500

    display name and description

--- src/components/PokeNameAndDescription.tsx
+++ src/components/PokeNameAndDescription.tsx
@@ -1,14 +1,16 @@
 import React, { useContext } from 'react';
-import { PokeContext } from '../pages/Pokepage/index';
+// import { PokeContext } from '../pages/Pokepage/index';
 import { Container, Box } from '@mui/material';
+import { useUpdatePokeContext, usePokeContext } from '../pages/Pokepage/PokeContext';
 
 const PokeNameAndDescription = () => {
 
-  const { name, id, types, height, weight } = useContext(PokeContext);
+  const { id, name, types, height, weight } = usePokeContext();
+  // const { name, id, types, height, weight } = useContext(PokeContext);
 
   return (
     <Container>
-      <Box className="pokemon-name">Name: {name}</Box>
+      <Box className="pokemon-name">Name: {name} </Box>
       <Box className="pokemon-description">
         Description: Number: {id} Types:{types?.[0].type.name} Height: {height} Weight:{weight}
       </Box>
