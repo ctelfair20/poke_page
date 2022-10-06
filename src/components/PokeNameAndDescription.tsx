7ab56c89072fa1fb1a0805c45bfe969f3d55b0e1
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-10-06 12:16:37 -0500

    adds a comment to name and description

--- src/components/PokeNameAndDescription.tsx
+++ src/components/PokeNameAndDescription.tsx
@@ -2,7 +2,7 @@ import { Container, Box } from '@mui/material';
 import { usePokeContext } from '../pages/Pokepage/PokeContext';
 
 const PokeNameAndDescription = () => {
-  // Typescript won't let me add these varibles together like normal strings -- add " || '' " to variables
+  // Typescript won't let me add these varibles together like normal strings -- add " || '' " to variables -- look for css  to do this easier
   const { id, name, types, height, weight } = usePokeContext();
   const nameFirstLetterCaps = name?.[0].toUpperCase();
   const remainingLetterInName = name?.slice(1);
