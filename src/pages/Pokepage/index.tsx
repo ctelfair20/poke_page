84b679731ae2a8b69ecfb63639b739fbb39feeb0
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-10-07 19:41:44 -0500

    reordered imports on Pokepage/index

--- src/pages/Pokepage/index.tsx
+++ src/pages/Pokepage/index.tsx
@@ -1,36 +1,30 @@
 import { Box, Container, Grid, Typography } from "@mui/material";
+import PokeProvider from "./PokeContext";
+import PokeCard from "../../components/PokeCard";
+import Stats from "../../components/Stats";
 import Abilities from "../../components/Abilities";
 import Attacks from "../../components/Attacks";
-import PokeCard from "../../components/PokeCard";
 import Pokeball from "../../components/Pokeball";
-import Stats from "../../components/Stats";
-import PokeProvider from "./PokeContext";
 
 const PokePage = () => {
 
   return (
     <PokeProvider>
       <Box className="pokemon-page">
-        {/* <Typography
-          variant="h4"
-          align="center"
-          gutterBottom
-        >
-          Pokemon Page
-        </Typography> */}
         {/* https://1000logos.net/wp-content/uploads/2017/05/Symbol-Pokemon-Logo.jpg -- black logo*/}
         <Box id="logo-box">
           <img id="logo" src="https://1000logos.net/wp-content/uploads/2017/05/Pokemon-logo.jpg"
             alt="pokemon-logo" />
         </Box>
-
         <Container>
+
           <Grid
             container
             direction="row"
             justifyContent="space-evenly"
             alignItems="center"
           >
+            {/* This grid item holds the pokemon's image, name and basic info */}
             <Grid
               item
               className="left-col"
@@ -53,8 +47,8 @@ const PokePage = () => {
                 <Attacks />
               </Grid>
             </Grid>
-
           </Grid>
+
         </Container>
         <Box className="pokeball-box">
           <Pokeball />
