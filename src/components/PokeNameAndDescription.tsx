40b9d8e4126530687a23d901a2d73d628b13f51f
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-10-10 18:04:23 -0500

    fixed name

--- src/components/PokeNameAndDescription.tsx
+++ src/components/PokeNameAndDescription.tsx
@@ -4,16 +4,33 @@ import { usePokeContext } from '../pages/Pokepage/PokeContext';
 const PokeNameAndDescription = () => {
   // Typescript won't let me add these varibles together like normal strings -- add " || '' " to variables -- look for css  to do this easier
   const { id, name, types, height, weight } = usePokeContext();
-  const nameFirstLetterCaps = name?.[0].toUpperCase();
-  const remainingLetterInName = name?.slice(1);
   const typeFirstLetterCaps = types?.[0].type?.name?.[0].toUpperCase();
-  const remainingLetterInType = types?.[0].type?.name?.slice(1);
+  const remainingLettersInName = name?.slice(1);
+  const remainingLettersInType = types?.[0].type?.name?.slice(1);
+
+
+  const checkName = (piece: string | undefined) => {
+    const nameFirstLetterCaps = name?.[0].toUpperCase();
+    const possName = `${nameFirstLetterCaps}${remainingLettersInName}`
+
+    piece = piece || '';
+    if (piece.indexOf('-') >= 0) {
+      const before = piece.slice(0, piece.indexOf('-'));
+      const after = piece.slice(piece.indexOf('-') + 1);
+      return `${nameFirstLetterCaps + before + after}`;
+    } else {
+      return possName;
+    }
+  }
 
   return (
     <Box className='below-image'>
-      <Paper elevation={3} className="pokemon-name">Name: {`${nameFirstLetterCaps}${remainingLetterInName}`} </Paper>
+      <Paper elevation={3} className="pokemon-name">{`${checkName(remainingLettersInName)}`}</Paper>
       <Paper elevation={3} className="pokemon-description">
-        Description: Number: {id} Types: {`${typeFirstLetterCaps}${remainingLetterInType}`} Height: {height} Weight: {weight}
+        <span id='description'>Description:</span>
+        <Box>
+          Number: {id} Types: {`${typeFirstLetterCaps}${remainingLettersInType}`} Height: {height} Weight: {weight}
+        </Box>
       </Paper>
     </Box>
   );
