7ad01b6bc97dd134ed3315f3c86c1f887a245740
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-10-12 13:48:02 -0500

    gave description two columns

--- src/components/PokeNameAndDescription.tsx
+++ src/components/PokeNameAndDescription.tsx
@@ -30,9 +30,9 @@ const PokeNameAndDescription = () => {
     <Box className='below-image'>
       <Paper elevation={3} className="pokemon-name">{possName}</Paper>
       <Paper elevation={3} className="pokemon-description">
-        <span id='description'>Description:</span>
-        <Box>
-          Number: {id} Types: {`${typeFirstLetterCaps}${remainingLettersInType}`} Height: {height} Weight: {weight}
+        <span id='description-title'>Description:</span>
+        <Box id='description'>
+          Number: {id} Types: {`${typeFirstLetterCaps}${remainingLettersInType}`} Height: {height} Weight:{weight}
         </Box>
       </Paper>
     </Box>
