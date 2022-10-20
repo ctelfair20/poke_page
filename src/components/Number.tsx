1312e6358ecfa0e90d9892a6f4c674e2616bc5ac
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-10-19 21:08:26 -0500

    made comment

--- src/components/Number.tsx
+++ src/components/Number.tsx
@@ -2,6 +2,7 @@ import { Box, Typography } from "@mui/material";
 import { usePokeContext } from '../pages/Pokepage/PokeContext';
 
 const Number = () => {
+  // should make new context
   const { id } = usePokeContext();
 
   return (
