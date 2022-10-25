5f42dcf81934458cf275904bb91c5362efc05979
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-10-25 13:15:50 -0500

    reorganized teamCard to display info from props

--- src/components/TeamCard.tsx
+++ src/components/TeamCard.tsx
@@ -3,36 +3,48 @@ import { useEffect, useState } from "react";
 import HelpIcon from '@mui/icons-material/Help';
 import { usePokeContext } from '../pages/Pokepage/PokeContext';
 import { usePokedexContext } from '../pages/Pokepage/PokeContext';
+import { pokeInterface } from '../pages/Pokepage/interface';
+
+// This component displays the mini art or official art for the current pokemon as a team memeber
 
 interface PropsI {
   id: number
   teamNumber: number
+  pokemon: pokeInterface
 }
 
-const TeamCard = ({ id, teamNumber }: PropsI) => {
+const TeamCard = ({ id, teamNumber, pokemon }: PropsI) => {
 
-  const pokedex = usePokedexContext();
-  const [matched, setMatch] = useState(false);
-  const { sprites, name } = usePokeContext();
-  const miniArt = sprites?.front_default;
-  const officialArt = sprites?.other['official-artwork']['front_default'];
+  // const pokedex = usePokedexContext();
+  // const [matched, setMatch] = useState(false);
+  // const { sprites, name } = usePokeContext();
+  const pokemonName = pokemon.name
+  const miniArt = pokemon.sprites?.front_default;
+  const officialArt = pokemon.sprites?.other['official-artwork']['front_default'];
 
-  useEffect(() => {
-    if (id === teamNumber) {
-      setMatch(true)
-    }
-  }, [teamNumber])
+  // useEffect(() => {
+  //   console.log(pokedex);
+
+  // }, [])
 
   return (
     <Box
       className="team-member"
     >
-      {/* <Tooltip
-        title="favorite a pokemon to add it to your team"
-        disableFocusListener
-      > */}
-      {matched ? <img className="mini-art" src={miniArt ? miniArt : officialArt} alt={name} /> : <HelpIcon />}
-      {/* </Tooltip> */}
+      {
+        // pokemon ?
+        <img
+          className="mini-art"
+          src={miniArt ? miniArt : officialArt}
+          alt={pokemonName}
+        />
+        // : <Tooltip
+        //   title="favorite a pokemon to add it to your team"
+        //   disableFocusListener
+        // >
+        //   <HelpIcon />
+        // </Tooltip>
+      }
     </Box>
   );
 }
