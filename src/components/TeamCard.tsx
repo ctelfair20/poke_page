911ec9ba1fc5337e6a170f5685b14d2cbbf34c6a
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-10-24 20:47:20 -0500

    post reset: displaying sprites bug fixed

--- src/components/TeamCard.tsx
+++ src/components/TeamCard.tsx
@@ -2,7 +2,7 @@ import { Box, Tooltip } from "@mui/material";
 import { useEffect, useState } from "react";
 import HelpIcon from '@mui/icons-material/Help';
 import { usePokedexContext } from '../pages/Pokepage/PokeContext';
-
+import { usePokeContext } from '../pages/Pokepage/PokeContext';
 
 interface PropsI {
   id: number
@@ -13,9 +13,9 @@ const TeamCard = ({ id, teamNumber }: PropsI) => {
 
   const pokedex = usePokedexContext();
   const [matched, setMatch] = useState(false);
-  const currentPokemon = pokedex[pokedex.length - 1]
-  const miniArt = currentPokemon.sprites?.front_default;
-  const officialArt = currentPokemon.sprites?.other["official-artwork"].front_default;
+  const { sprites, name } = usePokeContext();
+  const miniArt = sprites?.front_default;
+  const officialArt = sprites?.other['official-artwork']['front_default'];
 
   useEffect(() => {
     if (id === teamNumber) {
@@ -31,7 +31,7 @@ const TeamCard = ({ id, teamNumber }: PropsI) => {
         title="favorite a pokemon to add it to your team"
         disableFocusListener
       > */}
-      {matched ? <img className="mini-art" src={miniArt ? miniArt : officialArt} alt={currentPokemon.name} /> : <HelpIcon />}
+      {matched ? <img className="mini-art" src={miniArt ? miniArt : officialArt} alt={name} /> : <HelpIcon />}
       {/* </Tooltip> */}
     </Box>
   );
