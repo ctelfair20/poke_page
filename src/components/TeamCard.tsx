6ba4a1595dcaf8548874df0cc8ba0c3f6438a74f
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-10-26 15:46:34 -0500

    clean up

--- src/components/TeamCard.tsx
+++ src/components/TeamCard.tsx
@@ -1,49 +1,27 @@
-import { Box, Tooltip } from "@mui/material";
-import { useEffect, useState } from "react";
-import HelpIcon from '@mui/icons-material/Help';
-import { usePokeContext } from '../pages/Pokepage/PokeContext';
-import { usePokedexContext } from '../pages/Pokepage/PokeContext';
+import { Box } from "@mui/material";
 import { pokeInterface } from '../pages/Pokepage/interface';
 
 // This component displays the mini art or official art for the current pokemon as a team memeber
-
 interface PropsI {
-  // id: number
-  // teamNumber: number
   pokemon: pokeInterface
 }
 
 const TeamCard = ({ pokemon }: PropsI) => {
 
-  // const pokedex = usePokedexContext();
-  // const [matched, setMatch] = useState(false);
-  // const { sprites, name } = usePokeContext();
   const pokemonName = pokemon.name
   const miniArt = pokemon.sprites?.front_default;
   const officialArt = pokemon.sprites?.other['official-artwork']['front_default'];
-
-  // useEffect(() => {
-  //   console.log(pokedex);
-
-  // }, [])
 
   return (
     <Box
       className="team-member"
     >
       {
-        // pokemon ?
         <img
           className="mini-art"
           src={miniArt ? miniArt : officialArt}
           alt={pokemonName}
         />
-        // : <Tooltip
-        //   title="favorite a pokemon to add it to your team"
-        //   disableFocusListener
-        // >
-        //   <HelpIcon />
-        // </Tooltip>
       }
     </Box>
   );
