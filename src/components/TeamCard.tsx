84edcbbc04eb0a906e10bfd4b77d1e30d907579b
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-10-27 20:28:48 -0500

    display team member when clicked

--- src/components/TeamCard.tsx
+++ src/components/TeamCard.tsx
@@ -1,16 +1,25 @@
 import { Box } from "@mui/material";
 import { pokeInterface } from '../pages/Pokepage/interface';
+import { useUpdatePokeContext, usePokedexContext } from '../pages/Pokepage/PokeContext';
 
-// This component displays the mini art or official art for the current pokemon as a team memeber
+// This component displays the mini art or official art for the current pokemon as a team memeber IF the user favorited the pokemon
 interface PropsI {
   pokemon: pokeInterface
 }
 
 const TeamCard = ({ pokemon }: PropsI) => {
 
+  const pokemonInfoSetter = useUpdatePokeContext();
+  const pokedex = usePokedexContext();
+
   const pokemonName = pokemon.name
   const miniArt = pokemon.sprites?.front_default;
   const officialArt = pokemon.sprites?.other['official-artwork']['front_default'];
+
+  const handleClick = () => {
+    const index = pokedex.indexOf(pokemon);
+    pokemonInfoSetter(pokedex[index])
+  }
 
   return (
     <Box
@@ -19,6 +28,7 @@ const TeamCard = ({ pokemon }: PropsI) => {
       {
         <img
           className="mini-art"
+          onClick={handleClick}
           src={miniArt ? miniArt : officialArt}
           alt={pokemonName}
         />
