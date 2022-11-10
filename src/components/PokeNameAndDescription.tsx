f9e4a7c412f2099631cf53aaf5cb69f29dcd3d4b
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-11-09 22:04:38 -0600

    adds comments

--- src/components/PokeNameAndDescription.tsx
+++ src/components/PokeNameAndDescription.tsx
@@ -3,14 +3,13 @@ import { usePokeContext } from '../pages/Pokepage/PokeContext';
 
 const PokeNameAndDescription = () => {
   // Typescript won't let me add these varibles together like normal strings -- add " || '' " to variables -- look for css  to do this easier
-  const { id, name, types, height, weight } = usePokeContext();
-  const typeFirstLetterCaps = types?.[0].type?.name?.[0].toUpperCase();
+  const { name, types, height, weight } = usePokeContext();
+  // const typeFirstLetterCaps = types?.[0].type?.name?.[0].toUpperCase();
   // const typeArray = types
   const remainingLettersInName = name?.slice(1);
-  const remainingLettersInType = types?.[0].type?.name?.slice(1);
+  // const remainingLettersInType = types?.[0].type?.name?.slice(1);
   const nameFirstLetterCaps = name?.[0].toUpperCase();
   const possName = `${nameFirstLetterCaps}${remainingLettersInName}`
-
   // converts data from api to an array of types
   const typesArray = () => {
     return types?.map((typeObj) => {
